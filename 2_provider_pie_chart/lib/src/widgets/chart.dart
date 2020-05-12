18504c43ad7770d4e8956f7a857bfed132db6b14
akilaj-oc <akilajayasooriya@icloud.com>
2020-05-12 18:46:06 +0530

    Add pie chart

--- 2_provider_pie_chart/lib/src/widgets/chart.dart
+++ 2_provider_pie_chart/lib/src/widgets/chart.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_pie_chart/src/viewmodel/schedule.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class MyChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _mySchedule = Provider.of<MySchedule>(context);
-    return Text(
-      '${(_mySchedule.stateManagementTime * 100).round()}%',
-      style: TextStyle(
-          color: Colors.grey[800],
-          fontWeight: FontWeight.w900,
-          fontStyle: FontStyle.italic,
-          fontFamily: 'Open Sans',
-          fontSize: 40),
+    return PieChart(
+      PieChartData(
+        sections: [
+          PieChartSectionData(value: 50, color: Colors.red, title: 'foo'),
+          PieChartSectionData(
+              value: _mySchedule.stateManagementTime * 100,
+              color: Colors.amber,
+              title: 'boo'),
+          PieChartSectionData(value: 50, color: Colors.blue, title: 'bar'),
+          PieChartSectionData(
+              value: 50, color: Colors.greenAccent, title: 'far'),
+        ],
+        borderData: FlBorderData(
+          show: false,
+        ),
+      ),
     );
   }
 }
